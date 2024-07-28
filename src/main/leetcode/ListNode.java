204c28178ced4b175f3cfde9f474533b2924b9f7
Mohamad Rageh <rageh205@gmail.com>
2024-07-28 14:38:56 +0300

    Runtime { 0ms, Beats 100% } Memory { 43.63MB, Beats 74.83% }

---
+++ src/main/leetcode/ListNode.java
@@ -0,0 +1,7 @@
+public class ListNode {
+    int val;
+    Reverse_Linked_List_II_92.ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, Reverse_Linked_List_II_92.ListNode next) { this.val = val; this.next = next; }
+}
