b5dc82d13ddf8357048bfe050a698193f3218f04
Mohamad Rageh <rageh205@gmail.com>
2025-03-10 01:12:28 +0200

    Runtime { 1ms, Beats 72.76% } Memory { 41.74MB, Beats 41.67% }

--- src/main/leetcode/Decode_String_394.java
+++ src/main/leetcode/Decode_String_394.java
@@ -2,43 +2,31 @@ import java.util.Stack;
 
 public class Decode_String_394 {
 	public static String decodeString(String s) {
-		int countRight = 0;
 		String result = "";
 		Stack<String> stack = new Stack<>();
-
-		for (int i = s.length() - 1; i >= 0; i--) {
-			if (s.charAt(i) == ']') {
-				stack.push("]");
-				countRight++;
-			} else if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
-				if (countRight == 0) {
-					result = s.charAt(i) + result;
-				} else {
-					stack.push(String.valueOf(s.charAt(i)));
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) != ']') {
+				stack.push(String.valueOf(s.charAt(i)));
+			} else {
+				String str = "";
+				while (!stack.peek().equals("[")) {
+					str = stack.pop() + str;
 				}
-			} else if (s.charAt(i) == '[') {
-				StringBuilder s1 = new StringBuilder();
-				while (!stack.isEmpty() && !stack.peek().equals("]")) {
-					s1.append(stack.pop());
+				stack.pop();
+				String no = "";
+				while (!stack.isEmpty() && Character.isDigit(stack.peek().charAt(0))) {
+					no = stack.pop() + no;
 				}
-				if (!stack.isEmpty()) {
-					stack.pop();
+				int number = Integer.parseInt(no);
+				String str2 = "";
+				while (number-- != 0) {
+					str2 += str;
 				}
-				countRight--;
-				StringBuilder calc = new StringBuilder();
-				i--;
-				while (i >= 0 && Character.isDigit(s.charAt(i))) {
-					calc.insert(0, s.charAt(i--));
-				}
-				i++;
-				int count = Integer.parseInt(calc.toString());
-				String s2 = s1.toString().repeat(count);
-				if (countRight >= 1) {
-					stack.push(s2);
-				} else {
-					result = s2 + result;
-				}
+				stack.push(str2);
 			}
+		}
+		while (!stack.isEmpty()) {
+			result = stack.pop() + result;
 		}
 		return result;
 	}
